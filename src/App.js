45958067c30331ef3bb12d7a149a5aaac34b6924
Axel Hecht <axel@pike.org>
2018-03-02 19:15:01 +0100

    fluent flavored

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Edit Fluent messages</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="fluent-messages">
+          Replace this with fluent messages.
         </p>
       </div>
     );
